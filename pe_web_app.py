a73202c80f702165050c04a9f3feae6e8e6ec2bd
Goofyz <129243293+GoofyzHealth@users.noreply.github.com>
2023-12-13 19:34:40 +0700

    Update pe_web_app.py

--- pe_web_app.py
+++ pe_web_app.py
@@ -68,7 +68,7 @@ def preeklamsia_risk_level(input_data):
     predicted_class = int(prediction[0])
 
     if predicted_class == 1:
-        return 'Anda *tidak birisiko* untuk terkena preeklamsia'
+        return 'Anda *tidak berisiko* untuk terkena preeklamsia'
     elif predicted_class == 2:
         return 'Anda memiliki *risiko sedang* untuk terkena preeklamsia'
     else:
